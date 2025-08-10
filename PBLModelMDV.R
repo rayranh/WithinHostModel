9080d7442c60cdc14ff290b33d5edb8f07ecde45
rayanh <rtg5200@psu.edu>
2025-08-10 17:13:35 -0400

    create a model for PBL of MDV

---
+++ PBLModelMDV.R
@@ -0,0 +1,81 @@
+rm(list = ls())
+library(deSolve)  
+library(reshape2) 
+library(ggplot2)
+library(dplyr)
+sir_equations <- function(time, variables, parameters) {
+  with(as.list(c(variables, parameters)), {
+    dB <- -M*B_cells - beta*Cb*B_cells - beta_2*Ct*B_cells + (g1*(Cb+Ct)/(g2+(Cb+Ct))) # beta = rate of cytolytically infected B cells by Cb and Ct 
+    dCb <- M*B_cells +beta*Cb*B_cells + beta_2*Ct*B_cells - alpha*Cb 
+    dT <- -M*T_cells - nu_A*Cb*T_cells - nu_b*Ct*T_cells + (h1*(Cb+Ct)/(h2+(Cb+Ct)))
+    dAt <- M*T_cells + nu_A*Cb*T_cells + nu_b*Ct*T_cells - beta_2*Ct*At - beta*Cb*At  # beta = rate of activated T cells by Ct and Cb cells 
+    # have to develop LT first 
+    inputLt <- theta*(beta_2*Ct*At + beta*Cb*At)  #remove mu*Lt since only the last compartment should contribute to tumors  
+    dLt <- inputLt - lambda*Lt 
+    dLt2 <- lambda*Lt - lambda*Lt2 
+    dLt3 <- lambda*Lt2 - lambda*Lt3 
+    dLt4 <- lambda*Lt3 - lambda*Lt4
+    dLt5 <- lambda*Lt4 - lambda*Lt5 - mu*Lt5
+    dCt <- (1-theta)*(beta_2*Ct*At + beta*Cb*At) - alpha_2*Ct
+    dZ <- mu*Lt5
+    df <- -nu_F*Lt5*f
+    dIf <- nu_F*Lt5*f
+    return(list(c(dB, dCb, dT, dAt,dLt,dLt2, dLt3,dLt4, dLt5, dCt, dZ, df,dIf)))
+  }) 
+}
+
+#blah blah adding someing 
+parameters_values <- c( 
+  M = 0.005
+  , beta = 10.819e-4                #contact rate with B cells 
+  , beta_2 = 5e-4        #contact rate with T cells 
+  , nu_A = 0.05         #Activation rate of T cells by cytolytic B cells (hours)
+  , nu_b = 0.001         #Activation rate of T cells by cytolytic T cells (hours)
+  , nu_F =0.07          #Infection rate of follicular cells (hours)
+  , mu = 0.1          #Rate of Tumor Cells (every 72 hours)
+  , alpha =  0.01           #death rate of cytolytic B cells (every 33 hours)
+  , alpha_2 = 0.01         #death rate of cytolytic T cells (every 48 hours)
+  , theta = 0.8            #population of activated T cells 
+  , g1 =  0              #incoming B cells (every 15 hours)
+  , g2 =0.001    
+  , h1 = 0                     #incoming T cells / determined no incoming T cells 
+  , h2 = 10  
+  , lambda = 0.005          #adding delay, how long latent cell 'exposed' 
+)
+
+initial_values <- c( 
+  B_cells = 80 
+  , Cb = 0 
+  , T_cells =10
+  , At = 0 
+  , Lt = 0 
+  , Lt2 = 0 
+  , Lt3 = 0  
+  , Lt4 = 0  
+  , Lt5 = 0 
+  , Ct = 0
+  , Z = 0
+  , f =20 
+  , If =0 
+  
+) 
+
+time_values <- seq(0, 1000) # hours
+
+sir_values_1 <- ode(
+  y = initial_values,
+  times = time_values,
+  func = sir_equations,
+  parms = parameters_values 
+) 
+
+#added strings as factors because otherwise turns into factors for some reason 
+sir_values_1 <- as.data.frame(sir_values_1, stringsAsFactors = FALSE) 
+#turning it longways 
+df <- melt(sir_values_1, id.vars = "time") %>% filter(variable %in% c("B_cells", "T_cells"))
+
+ggplot(data = df, aes(x = time/24, y = value, group = variable, colour = variable )) + geom_line() + 
+  scale_color_manual(values = c("B_cells" = "black", "T_cells" = "red", "Cb"="green", "At" = "blue", "Lt5" = "purple", 
+                                "Ct" = "yellow", "Z" = "lightblue", "f" = "aquamarine", "If" = "pink")) +
+  labs(title = "WithinHost Delay", color = "Cell Type") + theme(legend.position = "right") + theme_minimal() + xlab(label = "Time (Days)") + ylab(label = "Cell Number")
+
