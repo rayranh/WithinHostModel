bc67d4890b5ce04ae95570b60b73ce3c2cf6b6ee
rayanh <rtg5200@psu.edu>
2025-08-14 10:15:51 -0400

    adding dashed lines and fixing colors

--- PayneRenniePBL.R
+++ PayneRenniePBL.R
@@ -21,7 +21,7 @@ PayneMeanLongDf <- melt(Payne_mean, id.vars = c("days", "meanInfBcell", "meanCtr
 PayneLongB <- melt(Payne, id.vars = c("days")) %>% filter(!is.na(value), variable %in% c("infectBCells", "BCellsControl")) 
 
 
-ggplot(data = PayneLongB) +geom_point(mapping =aes(x = days, y= value,group = variable, colour = variable), position = position_jitter(width = 0), size = 2) + 
+ggplot(data = PayneLongB) +geom_point(mapping =aes(x = days, y= value,group = variable, colour = variable), position = position_jitter(width = 0.55, height =0), size = 1.5) + 
   scale_y_log10(limits = c(100, 20000), breaks = c(100, 1000, 10000, 20000), minor_breaks = c(200, 300, 400, 500, 600, 700, 800, 900,2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000)) + 
   scale_x_continuous(limits = c(0,45), breaks = seq(0,45,by=5)) + geom_line(data = PayneMeanLongDf, mapping = aes(x = days, y = value, group = variable, colour = variable, linetype = variable)) + annotation_logticks(base = 10, sides = "l") +
-  scale_linetype_manual(values = c("antilog" = "dashed", "antilogCtrl"= "solid")) + theme()
+  scale_linetype_manual(values = c("antilog" = "dashed", "antilogCtrl"= "solid")) + scale_color_manual(values = c("antilog"="indianred2", "antilogCtrl"="mediumseagreen", "infectBCells"= "indianred2", "BCellsControl"= "mediumseagreen"))+ theme()
