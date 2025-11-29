0d2cf919807af242bf6d335225c755712295cbbf
rayanh <rtg5200@psu.edu>
2025-11-28 23:48:42 -0500

    added log scale and fixed feather values

--- MDV_WithinHost.R
+++ MDV_WithinHost.R
@@ -8,14 +8,12 @@ library(ggplot2)
 library(dplyr) 
 library(readxl)
 sir_equations <- function(time, variables, parameters) {
-  with(as.list(c(variables, parameters)), { #turning initial values and parms into vectors and then list and then applying to below equations 
+  with(as.list(c(variables, parameters)), { #turning initial values and parms into vectors and then list and then applying to below equations // include death of B cells + host response death apoptosis? 
     dB <- -M*B_cells - beta*Cb*B_cells - beta_2*Ct*B_cells + (g1*(Cb+Ct)/(g2+(Cb+Ct))) # beta = rate of cytolytically infected B cells by Cb and Ct 
     dCb <- M*B_cells +beta*Cb*B_cells + beta_2*Ct*B_cells - alpha*Cb 
     dT <- -M*T_cells - nu_A*Cb*T_cells - nu_b*Ct*T_cells + (h1*(Cb+Ct)/(h2+(Cb+Ct)))
-    dAt <- M*T_cells + nu_A*Cb*T_cells + nu_b*Ct*T_cells - beta_2*Ct*At - beta*Cb*At  # beta = rate of activated T cells by Ct and Cb cells 
-   # have to develop LT first 
-    inputLt <- theta*(beta_2*Ct*At + beta*Cb*At)  #remove mu*Lt since only the last compartment should contribute to tumors  
-    dLt <- inputLt - lambda*Lt 
+    dAt <- M*T_cells + nu_A*Cb*T_cells + nu_b*Ct*T_cells - beta_2*Ct*At - beta*Cb*At  # beta = rate of activated T cells by Ct and Cb cells    
+    dLt <- theta*(beta_2*Ct*At + beta*Cb*At)  - lambda*Lt 
     dLt2 <- lambda*Lt - lambda*Lt2 
     dLt3 <- lambda*Lt2 - lambda*Lt3 
     dLt4 <- lambda*Lt3 - lambda*Lt4
@@ -32,19 +30,19 @@ sir_equations <- function(time, variables, parameters) {
 # get rid of macrophages and add cB 
 parameters_values <- c( 
   M = 0
-  , beta = 10.819e-8                #contact rate with B cells 
-  , beta_2 = 5e-8                   #contact rate with T cells 
+  , beta = 7.108697e-7                 #contact rate with B cells 
+  , beta_2 = 2.239485e-7                   #contact rate with T cells 
   , nu_A = 0.05                     #Activation rate of T cells by cytolytic B cells (hours)
-  , nu_b = 0.001                    #Activation rate of T cells by cytolytic T cells (hours)
-  , nu_F =0.07                      #Infection rate of follicular cells (hours)
-  , mu = 0.1                        #Rate of Tumor Cells (every 72 hours)
-  , alpha =  0.055                   #death rate of cytolytic B cells (every 33 hours)
-  , alpha_2 = 0.01                  #death rate of cytolytic T cells (every 48 hours)
+  , nu_b = 0.08                    #Activation rate of T cells by cytolytic T cells (hours)
+  , nu_F = 9.380823                      #Infection rate of follicular cells (hours)
+  , mu = 0.5                        #Rate of Tumor Cells (every 72 hours)
+  , alpha = 0.55                   #death rate of cytolytic B cells (every 33 hours)
+  , alpha_2 = 0.55                  #death rate of cytolytic T cells (every 48 hours)
   , theta = 0.8                     #population of activated T cells 
-  , g1 = 10000                      #incoming B cells (every 15 hours)
-  , g2 = 5    
+  , g1 = 100                      #incoming B cells (every 15 hours)
+  , g2 = 10    
   , h1 =100                        #incoming T cells / determined no incoming T cells 
-  , h2 = 5  
+  , h2 = 10  
   , lambda = 0.000055                #adding delay, how long latent cell 'exposed' 
 )
 
@@ -75,17 +73,29 @@ sir_values_1 <- ode(
 ) 
 
 #added strings as factors because otherwise turns into factors for some reason 
-sir_values_1 <- as.data.frame(sir_values_1, stringsAsFactors = FALSE) 
+sir_values_1 <- as.data.frame(sir_values_1, stringsAsFactors = FALSE)  
+
+
+#getting total B and T cell proportions 
+sir_values_total <- sir_values_1 %>% mutate(total_lymphocyte = B_cells + T_cells )
+
 #turning it longways 
-df <- melt(sir_values_1, id.vars = "time") %>% filter(!variable %in% c("Lt", "Lt2", "Lt4", "Lt3")) 
-baigent2016 <- read_xlsx("/Users/rayanhg/Downloads/baigent2016.xlsx")
+df <- melt(sir_values_1, id.vars = "time") %>% filter(!variable %in% c("Lt", "Lt2", "Lt4", "Lt3", "f", "If")) 
+baigent2016 <- read_xlsx("/Users/rayanhg/Downloads/baigent2016.xlsx", 2 ) 
+
+df_feathers_only <- melt(sir_values_1, id.vars = "time") %>% filter(variable %in% c("If")) 
 
 
 ggplot(data = df, aes(x = time/24, y = value, group = variable, colour = variable )) + geom_line() + 
   scale_color_manual(values = c("B_cells" = "black", "T_cells" = "red", "Cb"="green", "At" = "blue", "Lt5" = "purple", 
-                                "Ct" = "yellow", "Z" = "lightblue", "f" = "aquamarine", "If" = "pink")) + 
-  geom_point(data = baigent2016, aes(x = time, y = mean_genomes), inherit.aes = FALSE, color = "red") + 
+                                "Ct" = "yellow", "Z" = "lightblue")) + 
   labs(title = "WithinHost Delay", color = "Cell Type") + theme(legend.position = "right") + 
-  theme_minimal() + xlab(label = "Time (Days)") + ylab(label = "Cell Number") 
+  theme_minimal() + xlab(label = "Time (Days)") + ylab(label = "Cell Number")  
 
-
+FFE_plot <- ggplot(data = df_feathers_only, aes(x = time/24, y = value, group = variable, colour = variable )) + geom_line() + 
+  scale_color_manual(values = c( "If" = "pink")) + 
+  geom_point(data = baigent2016, aes(x = time, y = mean_genomes), inherit.aes = FALSE, color = "red") + 
+  labs(title = "WithinHost Delay", color = "Cell Type")  + xlab(label = "Time (Days)") + 
+  ylab(label = "Cell Number")  +  scale_y_log10(limits = c(0.01, 10000000)) + 
+  theme(panel.grid = element_blank(), panel.background = element_blank(),   legend.text = element_text(size = 12),
+        legend.title = element_text(size = 12), axis.line = element_line(color = "black")) + labs( y = "Cell Number", x = "Time (Days)", title = "WithinHost - Blood (B and T total)") 
