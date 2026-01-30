dfbe61222f8f64a9b356e67d3c6d0ab9176a1e74
rayanh <rtg5200@psu.edu>
2026-01-30 16:01:09 -0500

    added new F and If class because adding Baigent, so had to add nu_f parameter now as well

--- NoRecruitmentModel.R
+++ NoRecruitmentModel.R
@@ -21,10 +21,12 @@ sir_equations <- function(time, variables, parameters) {
     dT <- -nu_a*Cb*T_cells - nu_a*Ct*T_cells  
     dAt <- nu_a*Cb*T_cells + nu_a*Ct*T_cells - beta_2*Ct*At - beta*Cb*At 
     Inf_At <- T_sus * (beta_2*Ct*At + beta*Cb*At)
-    dLt <- theta*Inf_At - lambda*Lt  
-    dCt <- (1-theta)*Inf_At - alpha_Ct*Ct
+    dLt <- theta*Inf_At  
+    dCt <- (1-theta)*Inf_At - alpha_Ct*Ct 
+    dF <- -nu_f*Lt*F
+    dIf <- nu_f*Lt*F
     
-    return(list(c(dB, dCb, dT, dAt, dLt, dCt))) 
+    return(list(c(dB, dCb, dT, dAt, dLt, dCt, dF, dIf))) 
   }) 
 }  
 
@@ -39,7 +41,8 @@ Likelihood <- function(params){
   pars["beta_2"]  <- exp(pars["beta_2"])
   pars["alpha"] <- exp(pars["alpha"]) 
   pars["alpha_Ct"] <- exp(pars["alpha_Ct"])
-  pars["T_sus"] <- plogis(pars["T_sus"])  
+  pars["T_sus"] <- plogis(pars["T_sus"])   
+  pars["nu_f"]  <- exp(pars["nu_f"]) 
 
   
 
@@ -59,21 +62,38 @@ Likelihood <- function(params){
            prob_Cyto_B = (Cb)/(Cb+Ct), prob_Cyto_T = (Ct/(Cb+Ct)))  %>% 
     filter(time %in% obs_hourspp38) 
   
- 
+  # baigent2016 times are in days; matched_time is hours 
+  ffe_dat <- baigent2016 %>%
+    transmute(time = time * 24, mean_genomes) 
+  
+  #matching model time to feathers time 
+  Infected_FFE<- results %>% dplyr::select("time", "If") %>% 
+    filter(time %in% matched_time) %>% arrange(time)  
+  
+  # join model predictions (If) to observed data by time
+  ffe_join <- ffe_dat %>%
+    left_join(Infected_FFE, by = "time")  # brings in If
+  
   
   pp38_dbinom <- pp38_dat %>% filter(time %in% obs_hourspp38) %>%
-    left_join(infprob %>% select(time, prob_Cyto, prob_Cyto_B, prob_Cyto_T), by = "time") %>% arrange(time)  
+    left_join(infprob %>% select(time, prob_Cyto, prob_Cyto_B, prob_Cyto_T), by = "time") %>% arrange(time)   
+  
+  LogLHoodStor <- 0 
   
   loglike_pp38 <- dbinom(pp38_dbinom$mean.pp38, prob = pp38_dbinom$prob_Cyto, size = 40000, log = TRUE) 
   
-  loglike_pp38_B <- dbinom(pp38_dbinom$Bcell_no, prob = pp38_dbinom$prob_Cyto_B, size = pp38_dbinom$mean.pp38, log = TRUE) 
+  LogLHoodStor <- LogLHoodStor + sum(loglike_pp38) 
   
-  loglike_pp38_T <- dbinom(pp38_dbinom$Tcell_no, prob = pp38_dbinom$prob_Cyto_T, size = pp38_dbinom$mean.pp38, log = TRUE) # am i double counting? Adding too much weight 
+  loglike_ffe <- dnorm(
+    log10(ffe_join$mean_genomes),
+    mean = log10(ffe_join$If),
+    sd = 1.15,
+    log = TRUE
+  ) 
   
-
+  LogLHoodStor <- LogLHoodStor + sum(loglike_ffe)
   
-  
-  return(-sum(loglike_pp38, loglike_pp38_B, loglike_pp38_T)) 
+  return(-LogLHoodStor)   
   
 } 
 
@@ -86,7 +106,8 @@ parameter_intervals <-list(
   beta_2  = log(c(1e-08, 1e-2)),
   alpha = log(c(0.0104, 0.041)), 
   alpha_Ct = log(c(0.0104,0.041)),
-  T_sus = qlogis(c(0.0002, 0.01116))
+  T_sus = qlogis(c(0.0002, 0.01116)), 
+  nu_f = log(c(0.006, 0.01))
 )
 #taken from baigent data  
 
@@ -95,11 +116,12 @@ parameter_intervals <-list(
 parameters_values <- c(
   beta   = log(6.951463e-07), 
   beta_2   = log(6.951463e-07),
-  nu_a   = 4.668718e-01,
+  nu_a   = 4.668718e-01, 
+  nu_f   = log(0.008),
   alpha  = log(0.0104), 
   alpha_Ct = log(0.0104),
   theta  = 0.8,
-  lambda = 0.02380952,
+  lambda = 0.005952381, #1/(24*7)  
   T_sus  = qlogis(0.005),
   Pb     = 0.03
 ) 
@@ -111,7 +133,9 @@ initial_values<- c(
   T_cells = 1.5e6/3,
   At      = 0,
   Lt      = 0,
-  Ct      = 0
+  Ct      = 0, 
+  F       = 400000, 
+  If      = 0 
 )
 ### TIME ### 
 
@@ -119,14 +143,15 @@ time_values <- seq(0, 1080, by = 1)
 
 #PP38 Time, Right now the time is only including some small amount 
 obs_hourspp38 <- c(72,96,120,144) 
+#matching to the wack hours of baigent2016
+matched_time <- c(3,6,10,17,20,26,33,38)*24 
 
 
 ## DATA ##  
 #cytolytic infection at a given time of B and T cells in Spleen, Thymus, Bursa 
 pp38_dat <- read_xlsx("~/Desktop/WithinHostModel/WithinHostModel/baigent1998.xlsx", sheet = 3, na = "NA") %>% 
   filter(!is.na(mean.pp38)) %>% select(time, mean.pp38, Bcell_no, Tcell_no)
-# baigent2016 <- read_xlsx("/Users/rayanhg/Downloads/baigent2016.xlsx", 2 ) %>% arrange(time)
-
+baigent2016 <- read_xlsx("/Users/rayanhg/Downloads/baigent2016.xlsx", 2 ) %>% arrange(time)
 
 
 #generate random parameters 
@@ -163,11 +188,12 @@ optim_for_alpha <- function(){
     beta_2   = exp(answeroptim$par["beta_2"]),
     alpha  = exp(answeroptim$par["alpha"]), 
     alpha_Ct  = exp(answeroptim$par["alpha_Ct"]),
-    T_sus  = plogis(answeroptim$par["T_sus"]),
+    T_sus  = plogis(answeroptim$par["T_sus"]), 
+    nu_f   = exp(answeroptim$par["nu_f"]),
     nu_a   = parameters_values["nu_a"],
     theta  = parameters_values["theta"],
     lambda = parameters_values["lambda"], 
-    Pb =  parameters_values["Pb"],
+    Pb =  parameters_values["Pb"], 
     Converged = answeroptim$convergence
     )
   
@@ -183,6 +209,6 @@ final_df <- purrr::map_df(1:n_per_alpha, ~optim_for_alpha())
 
 
 
-write.csv(final_df, file = "/Users/rayanhg/Desktop/WithinHostModel/CodeOutputsRandNum/jan_30_26_SuperSimpleModel_CytoCelldinom_addedCtdbinom.csv") 
+write.csv(final_df, file = "/Users/rayanhg/Desktop/WithinHostModel/CodeOutputsRandNum/jan_30_26_SuperSimpleModel_AddedBaigent2016.csv") 
 
 
